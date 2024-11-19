e0dd279f49d34f7ad2be58a1459f9279514ad99d
Kevin Manceau <kevin.manceau@gmail.com>
2024-11-19 16:50:19 +0100

    [SECURITY][WIP] Registration form working, missing password hashing

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    #[Route(path: '/register', name: 'myrig_register')]
+    public function register(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $registrationForm = $this->createForm(UserType::class);
+        $registrationForm->handleRequest($request);
+
+        if($registrationForm->isSubmitted() && $registrationForm->isValid()) {
+            $user = new User();
+            $user->setRoles(['ROLE_USER']);
+            $user->setUsername($registrationForm->get('username')->getData());
+            $user->setPassword($registrationForm->get('password')->getData());
+            $user->setAge($registrationForm->get('age')->getData());
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('myrig_register');
+        }
+
+        return $this->render('security/register.html.twig', [
+            'registrationForm' => $registrationForm->createView(),
+        ]);
+    }
+
     #[Route(path: '/login', name: 'myrig_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
---
+++ src/Form/UserType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'Username',
+                'attr' => ['class' => 'form-control my-2', 'placeholder' => 'Username'],
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Password',
+                'hash_property_path' => 'password',
+                'mapped' => false,
+                'attr' => ['class' => 'form-control my-2', 'placeholder' => 'Password'],        
+            ])
+            ->add('age', NumberType::class, [
+                'label' => 'Age',
+                'attr' => ['class' => 'form-control my-2', 'placeholder' => '00'],
+            ])
+            ->add('register', SubmitType::class, [
+                'label' => 'Register',
+                'attr' => ['class' => 'btn btn-primary mt-2'],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
