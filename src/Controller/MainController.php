22d30bd9795bb5a1c95b45734d15d5c61b184cb6
Kevin Manceau <kevin.manceau@gmail.com>
2024-11-20 15:27:52 +0100

    [PAGE] Preparing the Users Route and template

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,6 +14,16 @@ class MainController extends AbstractController
     {
         return $this->render('main/index.html.twig', [
 
+        ]);
+    }
+
+    #[Route('/users', name: 'myrig_users')]
+    public function users(UserRepository $userRepository): Response
+    {
+        $userList = $userRepository->findAll();
+
+        return $this->render('main/users.html.twig', [
+            'users' => $userList,
         ]);
     }
 }
